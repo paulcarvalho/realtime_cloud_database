76d6708f5e784e72e8e3926562a403c70312428d
Paul Carvalho <pcarvalh@ucsc.edu>
2022-12-21 09:45:54 -0800

    organize code and add shed tag detections to shed tags table

--- cloud_db_management.sql
+++ cloud_db_management.sql
@@ -4,26 +4,38 @@
 -- Description: Shed tag filter for detections table. 
 ------------------------------------------------------------------------------------------------------------------------------
 
--- Find tagcodes with > 20000 detections and > 20 days between first and last detection at a particular general_location
-SELECT TagCode, general_location, n_detects = COUNT(*), duration = DATEDIFF(day, MIN(DateTime_PST) , MAX(DateTime_PST)) -- n_detects = number of detections for each tagcode at a general_location; duration = number of days between the first and last detections
+-- 1. Find tagcodes with > 20000 detections and > 20 days between first and last detection at a particular general_location
+SELECT TagCode, general_location, n = COUNT(*), max = MAX(DateTime_PST), min = MIN(DateTime_PST), days = DATEDIFF(day, MIN(DateTime_PST) , MAX(DateTime_PST)) -- n_detects = number of detections for each tagcode at a general_location; duration = number of days between the first and last detections
 INTO #tmp_table1 -- insert into a temporary table for later use
 FROM detects_with_locations -- use this View 
 GROUP BY TagCode, general_location; -- aggregate by tagcode and general_location
 
-SELECT TagCode, general_location, n_detects, duration
+SELECT TagCode, general_location, n, max, min, days
 INTO #tmp_table2 -- insert into another temporary table for later use
 FROM #tmp_table1
-WHERE (n_detects > 20000 AND duration > 20); -- only get tagcode + general location when the number of detections is > 20000 and days between first/last detections is 20
+WHERE (n > 20000 AND days > 20); -- only get tagcode + general location when the number of detections is > 20000 and days between first/last detections is 20
 
--- Get all of the detections for shed tags identified in #tmp_table2
-SELECT recv_ID, TagCode, DateTime_PST
+    -- should remove #tmp_table1 here
+
+-- 2. Record information for shed tags identified in step 1.
+INSERT INTO shed_tags(TagCode, general_location, n, max, min, days)
+SELECT TagCode, general_location, n, max, min, days
+FROM #tmp_table2;
+
+-- 3. Get all of the detections for shed tags identified in #tmp_table2 and insert into a temporary table
+SELECT recv_ID, TagCode, general_location, DateTime_PST
 INTO #tmp_table3
-FROM detects
+FROM detects_with_locations d
 WHERE EXISTS(
             SELECT 1
             FROM #tmp_table2
-            WHERE #tmp_table2.TagCode = detects.TagCode
+            WHERE #tmp_table2.TagCode = d.TagCode AND
+            #tmp_table2.general_location = d.general_location
 );
+
+SELECT TOP(1000) recv_ID, TagCode, DateTime_PST FROM #tmp_table3
+
+-- Just save first 1000 for tag code at particular general location
 
 -- Get the first and last detections
 SELECT recv_ID, TagCode, DateTime_PST, 
@@ -44,6 +56,8 @@ DROP TABLE #tmp_table3;
 DROP TABLE #tmp_table4;
 DROP TABLE #tmp_table5;
 
+
+
 -- TEMPORARY CODE TO BE DELETED
 SELECT * FROM #tmp_table4;
 SELECT recv_ID, TagCode, DateTime_PST FROM #tmp_table3 ORDER BY TagCode, DateTime_PST;
@@ -53,3 +67,10 @@ SELECT TagCode, general_location, DateTime_PST
 FROM detects_with_locations
 WHERE TagCode = 'A555'
 ORDER BY DateTime_PST;
+
+CREATE NONCLUSTERED INDEX IX_detects_recvID_dateTime ON detects
+(
+    [recv_ID]
+)
+INCLUDE ([DateTime_PST])
+GO
