c0dea54e62335c194c38549233b6cc8d73a935a2
Paul Carvalho <pcarvalh@ucsc.edu>
2022-12-21 13:03:30 -0800

    script not running

--- shed_tag_filter.sql
+++ shed_tag_filter.sql
@@ -18,6 +18,7 @@ WHERE (n > 20000 AND days > 20); -- only get tagcode + general location when the
     -- should remove #tmp_table1 here
 
 -- 2. Record information for shed tags identified in step 1.
+BEGIN TRANSACTION
 INSERT INTO shed_tags (TagCode, general_location, n, max, min, days)
 SELECT TagCode, general_location, n, max, min, days
 FROM #tmp_table2
@@ -28,22 +29,6 @@ WHERE NOT EXISTS(
     AND shed_tags.general_location = #tmp_table2.general_location
     AND shed_tags.n = #tmp_table2.n
     AND shed_tags.max = #tmp_table2.[max]
-)
-
-
-BEGIN TRANSACTION
-INSERT INTO receivers (location, recv, general_location, latitude, longitude, rkm)
-SELECT DISTINCT location, recv, general_location, latitude, longitude, rkm
-FROM stage_detects
-WHERE NOT EXISTS(
-    SELECT 1
-    FROM receivers
-    WHERE receivers.recv = stage_detects.recv 
-    AND receivers.location = stage_detects.location
-    AND receivers.general_location = stage_detects.general_location
-    AND receivers.latitude = stage_detects.latitude
-    AND receivers.longitude = stage_detects.longitude
-    AND receivers.rkm = stage_detects.rkm
 )
 OPTION(USE HINT('ENABLE_PARALLEL_PLAN_PREFERENCE'))
 COMMIT TRANSACTION;
@@ -59,7 +44,64 @@ WHERE EXISTS(
             #tmp_table2.general_location = d.general_location
 );
 
-SELECT TOP(1000) recv_ID, TagCode, DateTime_PST FROM #tmp_table3
+-- 4 Save a temporary table with unique shed tag codes and number keys for batch processing below
+SELECT DISTINCT TagCode
+INTO #tmp_table4
+FROM #tmp_table3;
+
+ALTER TABLE #tmp_table4
+ADD ID INT IDENTITY(1,1);
+
+-- 5. Remove the all shed tags from detects table (Note, for run-time efficiency, this code removes records in batches instead of all records in #tmp_table3 at once)
+
+DECLARE
+	@LargestKeyProcessed INT = -1,
+	@NextBatchMax INT,
+	@RC INT = 1;
+ 
+WHILE (@RC > 0)
+BEGIN
+ 
+    SELECT TOP (2) @NextBatchMax = ID
+    FROM #tmp_table4
+    WHERE ID > @LargestKeyProcessed;
+
+    WITH cte_1 (TagCode, ID) AS (
+        SELECT TagCode, ID
+        FROM #tmp_table4
+        WHERE ID > @LargestKeyProcessed
+        AND ID <= @NextBatchMax
+    ),
+
+    cte_2 (recv_ID, TagCode, general_location, DateTime_PST) AS (
+        SELECT recv_ID, TagCode, general_location, DateTime_PST
+        FROM #tmp_table3
+        WHERE EXISTS(
+            SELECT 1
+            FROM cte_1
+            WHERE cte_1.TagCode = #tmp_table3.TagCode
+        )
+    )
+
+    DELETE FROM detects
+    WHERE EXISTS(
+    SELECT 1
+    FROM cte_2
+        WHERE cte_2.recv_ID = detects.recv_ID
+        AND cte_2.TagCode = detects.TagCode
+        AND cte_2.DateTime_PST = detects.DateTime_PST
+    );
+
+--   DELETE FactOnlineSales
+--   WHERE CustomerKey = 19036
+--     AND OnlineSalesKey > @LargestKeyProcessed
+--     AND OnlineSalesKey <= @NextBatchMax;
+--  
+    SET @RC = @@ROWCOUNT;
+    SET @LargestKeyProcessed = @NextBatchMax;
+END
+
+SELECT TOP(1000) * FROM #tmp_table3
 
 -- Just save first 1000 for tag code at particular general location
 
